fa776f59e2da08a6cdf49c7b5c8f17e2f464a0e2
DESKTOP-NNMT26G\UVBlu <rwrmail1@gmail.com>
2018-03-23 16:14:15 -0500

    Section 04, Lecture 124 - Delegating to Components

---
+++ BattleTankLowPoly/Source/BattleTankLowPoly/Private/TankPlayerController.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankPlayerController.h"
+
+
+ATank* ATankPlayerController::getControlledTank() const {
+
+	return(Cast<ATank>(GetPawn()));
+	
+};
+
